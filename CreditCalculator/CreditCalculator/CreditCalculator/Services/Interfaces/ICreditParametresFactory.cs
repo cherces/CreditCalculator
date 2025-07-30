984e95917c8f66b26e1de651022d90ca9d40b8e2
cherces <79788676+cherces@users.noreply.github.com>
2025-07-30 09:38:01 +0300

    .
    .

--- CreditCalculator/CreditCalculator/CreditCalculator/Services/Interfaces/ICreditParametresFactory.cs
+++
@@ -1,9 +0,0 @@
-using CreditCalculator.Models;
-using CreditCalculator.Models.Form;
-
-namespace CreditCalculator.Services.Interfaces;
-
-public interface ICreditParametersFactory
-{
-    CreditParametersBase CreateParameters(CreditParametersFormModel formModel);
-}
